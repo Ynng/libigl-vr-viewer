79f63c872f54e2d0285797130776b2fef21ee262
Kevin Huang <44907675+Ynng@users.noreply.github.com>
2020-07-21 19:45:55 -0400

    wip

--- include/utils.h
+++ include/utils.h
@@ -4,7 +4,6 @@
 #include <iostream>
 #include <iomanip> // setprecision
 #include <sstream> // stringstream
-#include <openvr.h>
 
 using namespace std;
 
@@ -12,7 +11,5 @@ using namespace std;
 
 string ftos(float f, int precision);	// float to string with 2-decimal precision
 string vftos(float* v, int precision);	// float vector to string with 2-decimal precisions
-string GetTrackedDeviceString(vr::IVRSystem*, vr::TrackedDeviceIndex_t, vr::TrackedDeviceProperty, vr::TrackedPropertyError *peError = NULL);
-string GetTrackedDeviceClassString(vr::ETrackedDeviceClass td_class);
 
 #endif
--- main.cpp
+++ main.cpp
@@ -14,35 +14,35 @@ bool key_down(igl::opengl::glfw::Viewer& viewer, unsigned char key, int modifier
 	std::cout << "detected" << key << std::endl;
 	if (key == 'E')
 	{
-		viewer.core.camera_translation = viewer.core.camera_translation + Eigen::Vector3f(0.05, 0, 0);
+		viewer.core().camera_translation = viewer.core().camera_translation + Eigen::Vector3f(0.05, 0, 0);
 	}
 	if (key == 'Q')
 	{
-		viewer.core.camera_translation = viewer.core.camera_translation + Eigen::Vector3f(-0.05, 0, 0);
+		viewer.core().camera_translation = viewer.core().camera_translation + Eigen::Vector3f(-0.05, 0, 0);
 	}
 	if (key == 'A')
 	{
-		viewer.core.camera_translation = viewer.core.camera_translation + Eigen::Vector3f(0, 0.05, 0);
+		viewer.core().camera_translation = viewer.core().camera_translation + Eigen::Vector3f(0, 0.05, 0);
 	}
 	if (key == 'D')
 	{
-		viewer.core.camera_translation = viewer.core.camera_translation + Eigen::Vector3f(0, -0.05, 0);
+		viewer.core().camera_translation = viewer.core().camera_translation + Eigen::Vector3f(0, -0.05, 0);
 	}
 	if (key == 'Z')
 	{
-		viewer.core.camera_translation = viewer.core.camera_translation + Eigen::Vector3f(0, 0, 0.05);
+		viewer.core().camera_translation = viewer.core().camera_translation + Eigen::Vector3f(0, 0, 0.05);
 	}
 	if (key == 'C')
 	{
-		viewer.core.camera_translation = viewer.core.camera_translation + Eigen::Vector3f(0, 0, -0.05);
+		viewer.core().camera_translation = viewer.core().camera_translation + Eigen::Vector3f(0, 0, -0.05);
 	}
 	if (key == '4')
 	{
-		viewer.core.trackball_angle = viewer.core.trackball_angle * Eigen::Quaternionf(0.1, 0, 0, 0);
+		viewer.core().trackball_angle = viewer.core().trackball_angle * Eigen::Quaternionf(0.1, 0, 0, 0);
 	}
 	if (key == '5')
 	{
-		viewer.core.trackball_angle = viewer.core.trackball_angle * Eigen::Quaternionf(-0.1, 0, 0, 0);
+		viewer.core().trackball_angle = viewer.core().trackball_angle * Eigen::Quaternionf(-0.1, 0, 0, 0);
 	}
 	return false;
 }
@@ -54,13 +54,29 @@ int main(int argc, char *argv[])
 	// Load in a mesh
 	igl::read_triangle_mesh(argc > 1 ? argv[1] : "../shared/data/bunny.off", V, F);
 
+	unsigned int left_view, right_view;
 
 	// Create a libigl Viewer object 
 	igl::opengl::glfw::Viewer viewer;
 	// Set the vertices and faces for the viewer
 	viewer.data().set_mesh(V, F);
 
+
+	igl::opengl::VRApplication vr;
+
+	viewer.callback_init = [&](igl::opengl::glfw::Viewer&)
+	{
+		viewer.core().viewport = Eigen::Vector4f(1500, 0, 640, 800);
+		left_view = viewer.core_list[0].id;
+		//right_view = viewer.append_core(Eigen::Vector4f(640, 400, 640, 800));
+		right_view = viewer.append_vrcore(vr);
+		return false;
+	};
+
 	viewer.callback_key_down = &key_down;
+
+
+	
 
 
 	// Launch a viewer instance
